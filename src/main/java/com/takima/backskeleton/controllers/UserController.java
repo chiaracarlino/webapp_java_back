c1cd736cd2755997edd9927f7f7ec5890103d3d4
lucille <lucille.finet@epfedu.fr>
2025-10-11 16:05:42 +0200

    fonctionnement des requetes postman

--- src/main/java/com/takima/backskeleton/controllers/UserController.java
+++ src/main/java/com/takima/backskeleton/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.takima.backskeleton.controllers;
 import com.takima.backskeleton.models.User;
 import com.takima.backskeleton.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,8 +39,34 @@ public class UserController {
         return userService.updateUser(user);
     }
 
+    @PatchMapping("/{id}")
+    public User patchUser(@PathVariable Long id, @RequestBody User partialUser) {
+        return userService.patchUser(id, partialUser);
+    }
+
+
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
+
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<Void> options() {
+        return ResponseEntity
+                .ok()
+                .allow(
+                        HttpMethod.GET,
+                        HttpMethod.POST,
+                        HttpMethod.PUT,
+                        HttpMethod.PATCH,
+                        HttpMethod.DELETE,
+                        HttpMethod.OPTIONS,
+                        HttpMethod.HEAD
+                )
+                .build();
+    }
 }
+
+
+
