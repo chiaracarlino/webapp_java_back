7f13274e049243bd542546ee36d107339b6e6755
lucille <lucille.finet@epfedu.fr>
2025-10-13 13:08:36 +0200

    fonctionnement des requetes postman portfolio mais il faut revoir certaines logiques

--- src/main/java/com/takima/backskeleton/controllers/TemplateController.java
+++ src/main/java/com/takima/backskeleton/controllers/TemplateController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/templates")
 public class TemplateController {
